e903b2d31f999cefd4eddd99fc63fd19906964ea
robin-england <robin.england94@googlemail.com>
2024-06-07 14:03:26 -0400

    Add hover interactivity to div elements.

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// 3. Implement the JavaScript:
-// Write a function to initialize your page that creates div elements as data points and attaches interactive behaviors using D3.js.
-// Attach event listeners for "mouseover" and "mouseout" to change the div elements' styles on hover and revert them when the mouse moves away.
+// U63166005
+// Coding Challenge #9
 
 function main() {
 
-    d3.select("body").append("div")
+    d3.select("body").append("div") // Creates 3 div elements
     d3.select("body").append("div")
     d3.select("body").append("div")
 
     d3.selectAll("div")
         .on("mouseover", function(){
             d3.select(this)
-                .style()
+                .style("background-color","burlywood")  // Changes color on mouseover
         })
         .on("mouseout", function(){
             d3.select(this)
-                .style()
+                .style("background-color","bisque")     // Changes color back on mouseout
         })
 }
